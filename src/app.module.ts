6d1b84e8d4f5bd148e5ee7293fd0056e6da0103f
Finley <hongfeiz@mun.ca>
2023-06-14 17:08:51 -0300

    setting up..

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { EmailNotificationModule } from './email-notification/email-notification.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, AuthModule, EmailNotificationModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,15 @@
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+// import { AuthDto } from './dto';
+@Controller('auth')
+export class AuthController {
+    constructor(private authService: AuthService) {}
+    @Post('signup')
+    signup(@Body() dto:  any) {
+        return this.authService.signup(dto);
+    }
+    @Post('signin')
+    signin(@Body() dto: any) {
+        return this.authService.login(dto);
+    }
+}
